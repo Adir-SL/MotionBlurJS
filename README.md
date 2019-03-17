644219b6dcd279d6fd7375225f7aa3b9d8b40325
Adir <42638312+Adir-SL@users.noreply.github.com>
2019-03-17 09:50:29 +0200

    JavaScript library for motion blur
    Lightweight JavaScript library that allows you to add motion blur to your CSS animations

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# MotionBlurJS
+# **MotionBlurJS**
+## What is **MotionBlurJS**
+**MotionBlurJS** is a lightweight JavaScript library that allows your animations to shine with homemade native CSS Motion Blur and Motion Fade while keeping your code clean and your CSS animations smooth.
+
+## How does it work
+**MotionBlurJS** will duplicate your objects so they can get blurred or faded on runtime, the code will compile into a clean version of the motion blur and the DOM will only show the necessary objects, in order for it to work properly the blurred object has to be positioned absolutely and to be animated with keyframes CSS animation
+
+## The simple method
+You can use *MotionBlurJS* by simply linking to it in your document head tag like so:
+```HTML
+<head>
+    <script src="https://www.motionblurjs.com/blur.js"></script>
+</head>
+```
+After that, all you have to do is naming the class of the div you want to blur to either the **blurjs** or **fadejs** class, you can mix it with your current classes as well by separating those with spaces, like so:
+```HTML
+<div id="yourId" class="someClass blurjs"></div>
+```
+or
+```HTML
+<div id="yourId" class="fadejs someClass"></div> 
+```
+## The dynamic method
+This method will allow you to dynamically blur the objects even if they won’t load with the page itself, to achieve this just add this img tag inside the div you want to blur, like so:
+```HTML
+<div id="yourId" class="someClass">
+    <img src="" onerror="blurMe(event);" style="display:none !important;" />
+</div>
+```
+or
+```HTML
+<div id="yourId" class="someClass">
+    <img src="" onerror="fadeMe(event);" style="display:none !important;" />
+</div>
+```
+
+## Bug report
+This code is under active development, if you experience any bugs or kinks, feel free to report those through Github or by mail: motionblurjs@gmail.com please mention which method you were using and if possible, send me your code snippets, happy coding !
---
+++ blur.js
@@ -0,0 +1,59 @@
+function whenDone(){
+    var x = document.getElementsByClassName("blurjs");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if(window.runScriptA !== 1){
+            var y = x[i].parentNode.children;
+            var r;
+            for (r = 0; r < y.length; r++) {
+                if(y[r].className.search("blurjs") >= 0){
+                    var justTemp_1 = y[r].outerHTML.replace(/class=/, "style='animation-delay:0ms;' class=");
+                    var justTemp_2 = y[r].outerHTML.replace(/class=/, "style='animation-delay:20ms;' class=");
+                    var justTemp_3 = y[r].outerHTML.replace(/class=/, "style='animation-delay:40ms;' class=");
+                    var justTemp_4 = y[r].outerHTML.replace(/class=/, "style='animation-delay:60ms;' class=");
+                    y[r].outerHTML = justTemp_1+justTemp_2+justTemp_3+justTemp_4;
+                    r += 3;
+                }
+            }
+            window.runScriptA = 1;
+        }    
+    }
+    var x = document.getElementsByClassName("fadejs");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if(window.runScriptB !== 1){
+            var y = x[i].parentNode.children;
+            var r;
+            for (r = 0; r < y.length; r++) {
+                if(y[r].className.search("fadejs") >= 0){
+                    var justTemp_1 = y[r].outerHTML.replace(/class=/, "style='animation-delay:0ms;opacity:1;' class=");
+                    var justTemp_2 = y[r].outerHTML.replace(/class=/, "style='animation-delay:20ms;opacity:.75;' class=");
+                    var justTemp_3 = y[r].outerHTML.replace(/class=/, "style='animation-delay:40ms;opacity:.5;' class=");
+                    var justTemp_4 = y[r].outerHTML.replace(/class=/, "style='animation-delay:60ms;opacity:.25;' class=");
+                    y[r].outerHTML = justTemp_1+justTemp_2+justTemp_3+justTemp_4;
+                    r += 3;
+                }
+            }
+            window.runScriptB = 1;
+        }
+    }
+}
+document.addEventListener("DOMContentLoaded", whenDone);
+function blurMe(e){
+    var c = e.target.parentNode;
+    c.getElementsByTagName("img")[0].outerHTML="";
+    var justTemp_1 = c.outerHTML.replace(/class=/, "style='animation-delay:0ms;' class=");
+    var justTemp_2 = c.outerHTML.replace(/class=/, "style='animation-delay:20ms;' class=");
+    var justTemp_3 = c.outerHTML.replace(/class=/, "style='animation-delay:40ms;' class=");
+    var justTemp_4 = c.outerHTML.replace(/class=/, "style='animation-delay:60ms;' class=");
+    c.outerHTML = justTemp_1+justTemp_2+justTemp_3+justTemp_4;
+}
+function fadeMe(e){
+    var c = e.target.parentNode;
+    c.getElementsByTagName("img")[0].outerHTML="";
+    var justTemp_1 = c.outerHTML.replace(/class=/, "style='animation-delay:0ms;opacity:1;' class=");
+    var justTemp_2 = c.outerHTML.replace(/class=/, "style='animation-delay:20ms;opacity:.75;' class=");
+    var justTemp_3 = c.outerHTML.replace(/class=/, "style='animation-delay:40ms;opacity:.5;' class=");
+    var justTemp_4 = c.outerHTML.replace(/class=/, "style='animation-delay:60ms;opacity:.25;' class=");
+    c.outerHTML = justTemp_1+justTemp_2+justTemp_3+justTemp_4;
+}
