783a7cd73c17b43c01fd8e7dda39f3c503d89a3f
Adir <slutzkis@gmail.com>
2024-05-05 16:38:05 +0300

    Update blur.js

--- blur.js
+++ blur.js
@@ -142,7 +142,7 @@ function blurMe(e) {
 
     var i;
     for (i = 1; i <= g; i++) {
-        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms; class=");
+        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms;transition-delay:" + (g - i) * 20 + "ms; class=");
         temps = temps + temp;
     }
     c.outerHTML = temps;
@@ -162,7 +162,7 @@ function fadeMe(e) {
 
     var i;
     for (i = 1; i <= g; i++) {
-        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms;opacity:" + (100 - (g - i) * (100 / g)) + "%;' class=");
+        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms;transition-delay:" + (g - i) * 20 + "ms;opacity:" + (100 - (g - i) * (100 / g)) + "%;' class=");
         temps = temps + temp;
     }
     c.outerHTML = temps;
