41de02340ba68ccfa94603de11b35043644462d6
Adir <slutzkis@gmail.com>
2024-07-22 12:10:01 +0300

    Update mb-react.js

--- mb-react.js
+++ mb-react.js
@@ -1,5 +1,42 @@
-function hello() {
-    console.log("HHEELLOO")
+function blurMe(e) {
+    var c = e.target.parentNode;
+    if (c.outerHTML.indexOf("blurjs-ghost") > 0) {
+        var g = Number(c.getAttribute("blurjs-ghost"));
+    } else {
+        var g = 4;
+    }
+
+    var temps = "";
+    c.getElementsByTagName("img")[0].outerHTML = "";
+    var d = c.outerHTML;
+
+    var i;
+    for (i = 1; i <= g; i++) {
+        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms;transition-delay:" + (g - i) * 20 + "ms; class=");
+        temps = temps + temp;
+    }
+    c.outerHTML = temps;
 }
-// module.exports = {whenDone, fadeMe, blurMe};
-module.exports = { hello }
+
+function fadeMe(e) {
+    var c = e.target.parentNode;
+    if (c.outerHTML.indexOf("blurjs-ghost") > 0) {
+        var g = Number(c.getAttribute("blurjs-ghost"));
+    } else {
+        var g = 4;
+    }
+
+    var temps = "";
+    c.getElementsByTagName("img")[0].outerHTML = "";
+    var d = c.outerHTML;
+
+    var i;
+    for (i = 1; i <= g; i++) {
+        var temp = d.replace(/class=/, "style='animation-delay:" + (g - i) * 20 + "ms;transition-delay:" + (g - i) * 20 + "ms;opacity:" + (100 - (g - i) * (100 / g)) + "%;' class=");
+        temps = temps + temp;
+    }
+    c.outerHTML = temps;
+}
+
+
+module.exports = { blurMe, fadeMe };
